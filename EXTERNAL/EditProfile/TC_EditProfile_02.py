6981f5491eced982740cad21efbe6f06d67b6470
Supapich <150766111+SUPAPICH16@users.noreply.github.com>
2025-09-27 12:21:02 +0700

    fix: update driver URL to production link

--- EXTERNAL/EditProfile/TC_EditProfile_02.py
+++ EXTERNAL/EditProfile/TC_EditProfile_02.py
@@ -16,21 +16,17 @@ options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
-driver.get("http://localhost:3000")
-
-driver.add_cookie({
-    "name": "authjs.session-token",
-    "value": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoibGNHbXhocGltT3FvM3loZU1VYi0zUENJaGFJeWpGdWwxMUVnbF82aldITEpfUzIxOXJmZmRXNlZvWFZqbWVnaVNvdEh0MjdlbEhDU3JmcUkxMTh5SEEifQ..9fX6KrmuzMO6IECh7UpK9Q.24b09nFhBRzakhh7jy5HSkIt927Wqst0s1e5G9LJf22Sl6IbwtzgNesMurX7han-aTiNU72mBO7lNW1Z7RrRyFD3aWK_7aYCa06v3iCPId5yzC6MXD9u_uotMUNZHmhBeGx96CcLgv7901xBNEK7o9EFyVqWNewwXC2WTn5evSMEUqZjHUFFxdQizE7V3Lak0muE9ZObOmab3VQShYxDt212aEw_zwO6x1ivR0mxjiqFdITM4rt83S_5G2XzFJm2sCPqWc14g-mQlCPfsHrr6GETe1RDqRbfCoz3mhSlwClfZdmgW8aLHLjAr_NdAnyuWbXvqRBlLI_0RMs74C3TnWizC-a3kowqsW90HOnSju6YmbYjKNHr5_wNXk9-0w_i.GJpKFJcW1GPGimq4Rb0URBW5foqIHogK0Fq2aIV0fQY",
-    "path": "/",
-})
-
 try:
-    #เปิดเว็บไซต์ และเช็กว่าเปิดแล้ว
-    driver.get("http://localhost:3000/role3-external")
+    driver.get("https://karuphan-hospital-production.up.railway.app/")
     
-    signin = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH,"//p[contains(@class, 'text-white')]"))
-    ).text.strip()
+    signup = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'เข้าสู่บัญชีของคุณ')]"))).text.strip()
+    assert signup == "เข้าสู่บัญชีของคุณ"
+    print("✅ Check the success words")
+
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/input").send_keys("weeraphat.s@example.com")
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/div[1]/input").send_keys("weeraphat1234")
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/button").click()
     time.sleep(2)
 
     role = WebDriverWait(driver, 10).until(
--- INTERNAL/EditProfile/TC_EditProfile_02.py
+++ INTERNAL/EditProfile/TC_EditProfile_02.py
@@ -16,23 +16,19 @@ options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
-driver.get("http://localhost:3000")
-
-driver.add_cookie({
-    "name": "authjs.session-token",
-    "value": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoibGNHbXhocGltT3FvM3loZU1VYi0zUENJaGFJeWpGdWwxMUVnbF82aldITEpfUzIxOXJmZmRXNlZvWFZqbWVnaVNvdEh0MjdlbEhDU3JmcUkxMTh5SEEifQ..eoAelVwdW1lTWMHwZNxV9A.SFc_FtY7B0-fDEOXWBodWAMSef371u7Xw-tOfNlOvawjJqB0PFz1su6qfBTjfZjbHIiDNYDzpjt6ze0Si98HkvejU_um0CvjRaBYhU1KkCU9Jm6pJDO_mImbI8fV6Z8k92RnQTnfFY0TLZ5-8NGl1w_TJNv91mSnV6XdWVS7MDlUF9a6kDL-Z3eml4GxMrbqAYxraD6iaeLvZyR2ae1EojMdKZaT5ZFNnJL66iyyKzzJT54r6cqiaDDpimVa_L_-PKALCIYQ74aY0pzYz9bB890iXPYo8Vyg1Qjpx2Oo8Oa23opY02vBkV0WAZWe5zfiytW2BhYIiYUvRTo_k09Jq7mbtvfGrlqiwszzMKAhOUUWcAGSWq--jt-WD3EXzAPyN2VauwoRbZ3QKtNLGfycIfqyVhWJ9xXqQRuD-4nmPx0.6IWiGKtlHwyQ7ngwJrAMPSY0V-RhjVJn2EOUH4fz4jI",
-    "path": "/",
-})
-
 try:
-    #เปิดเว็บไซต์ และเช็กว่าเปิดแล้ว
-    driver.get("http://localhost:3000/role2-internal")
+    driver.get("https://karuphan-hospital-production.up.railway.app/")
     
-    signin = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH,"//p[contains(@class, 'text-white')]"))
-    ).text.strip()
-    time.sleep(2)
+    signup = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'เข้าสู่บัญชีของคุณ')]"))).text.strip()
+    assert signup == "เข้าสู่บัญชีของคุณ"
+    print("✅ Check the success words")
 
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/input").send_keys("napass.sirikarn@gmail.com")
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/div[1]/input").send_keys("napass1234")
+    driver.find_element(By.XPATH,"/html/body/div[1]/form/button").click()
+    time.sleep(2)
+    
     role = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//div[2]/p[2]"))).text
     assert role == "กลุ่มงานบริการด้านปฐมภูมิและองค์รวม"
